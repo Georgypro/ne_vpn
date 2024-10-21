e7390e01a798adf9ee672370bd11e3209e8d9050
Mikhail <49973875+Chajka42@users.noreply.github.com>
2024-10-21 12:53:32 +0300

    promocode update

--- src/js/elements/Pricing.js
+++ src/js/elements/Pricing.js
@@ -1,104 +1,106 @@
 import React, { useState, useEffect } from 'react';
 import {BsStars} from "react-icons/bs";
 import '../../css/Pricing.css'
+import {useNavigate} from "react-router-dom";
+import {HiOutlineArrowSmallRight} from "react-icons/hi2";
 
 const Pricing = () => {
+    const [shopData, setShopData] = useState([]);
+    const navigate = useNavigate();
+    const [isPromoInput, setIsPromoInput] = useState(false);
+    const [promo, setPromo] = useState("");
+    const [msgFromPromo, setMsgFromPromo] = useState("");
 
-    const pay = (price) => {
-        const widget = new window.cp.CloudPayments();
-        const receipt = {
-            Items: [
-                {
-                    label: 'Доступ к сервису gostlink.ru',
-                    price: price,
-                    quantity: 1.00,
-                    amount: price,
-                    vat: 20,
-                    method: 0,
-                    object: 0,
-                }
-            ],
-            taxationSystem: 0,
-            email: 'pk_4a8b35334f78ae1f4ca1dbd3650c4',
-            phone: '',
-            isBso: false,
-            amounts: {
-                electronic: price,
-                advancePayment: 0.00,
-                credit: 0.00,
-                provision: 0.00
-            }
+    useEffect(() => {
+        const requestData = {
+            uid: localStorage.getItem('uid'),
+            email: localStorage.getItem('email'),
         };
+        fetchShopData(requestData);
+    }, []);
 
-        const data = {
-            CloudPayments: {
-                CustomerReceipt: receipt,
-                recurrent: {
-                    interval: 'Month',
-                    period: 1,
-                    customerReceipt: receipt
-                }
-            }
-        };
+    const fetchShopData = (userData) => {
 
-        widget.charge({
-                publicId: 'pk_4a8b35334f78ae1f4ca1dbd3650c4',
-                description: 'Подписка на ежемесячный доступ к сервису gostlink.ru',
-                amount: price,
-                currency: 'RUB',
-                accountId: localStorage.getItem('email'),
-                invoiceId: localStorage.getItem('uid'),
-                data: data
+        fetch(`https://gostlink.ru/shop/getShop`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            function (options) {
-                if (!window.kmpJsBridge) {
-                    console.warn("kmpJsBridge not found. Doing nothing.");
-                    return;
+            body: JSON.stringify(userData),
+        })
+            .then(response => {
+                console.log(response);
+                return response.text();
+            })
+            .then(text => {
+                console.log(text);
+                if (text) {
+                    return JSON.parse(text);
                 }
-                const data = {
-                    success: true,
-                    options: options,
-                    reason: 'no_reason'
-                };
-                window.kmpJsBridge.callNative(
-                    "pay",
-                    JSON.stringify(data),
-                    function (data) {
-                        document.getElementById("subtitle").innerText = data;
-                        console.log("Response from Native: " + data);
-                    }
-                );
-            },
-            function (reason, options) {
-                if (!window.kmpJsBridge) {
-                    console.warn("kmpJsBridge not found. Doing nothing.");
-                    return;
+                return {};
+            })
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    setShopData(data);
+                } else {
+                    navigate(`/`);
                 }
-                const data = {
-                    success: true,
-                    options: options,
-                    reason: reason
-                };
-                window.kmpJsBridge.callNative(
-                    "pay",
-                    JSON.stringify(data),
-                    function (data) {
-                        document.getElementById("subtitle").innerText = data;
-                        console.log("Response from Native: " + data);
-                    }
-                );
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+                navigate(`/`);
             });
     };
-
-    const payYear = (price) => {
+    const fetchPromo = () => {
+        const promoData = {
+            email: localStorage.getItem('email'),
+            promo: promo,
+        };
+        fetch(`https://gostlink.ru/shop/applyPromo`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(promoData),
+        })
+            .then(response => {
+                console.log(response);
+                return response.text();
+            })
+            .then(text => {
+                console.log(text);
+                if (text) {
+                    return JSON.parse(text);
+                }
+                return {};
+            })
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    const requestData = {
+                        uid: localStorage.getItem('uid'),
+                        email: localStorage.getItem('email'),
+                    };
+                    fetchShopData(requestData)
+                } else {
+                    setMsgFromPromo(data.message);
+                }
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+                navigate(`/`);
+            });
+    };
+    const pay = (firstPrice, subPrice) => {
         const widget = new window.cp.CloudPayments();
         const receipt = {
             Items: [
                 {
                     label: 'Доступ к сервису gostlink.ru',
-                    price: price,
+                    price: firstPrice,
                     quantity: 1.00,
-                    amount: price,
+                    amount: firstPrice,
                     vat: 20,
                     method: 0,
                     object: 0,
@@ -109,7 +111,7 @@ const Pricing = () => {
             phone: '',
             isBso: false,
             amounts: {
-                electronic: price,
+                electronic: firstPrice,
                 advancePayment: 0.00,
                 credit: 0.00,
                 provision: 0.00
@@ -130,7 +132,7 @@ const Pricing = () => {
         widget.charge({
                 publicId: 'pk_4a8b35334f78ae1f4ca1dbd3650c4',
                 description: 'Подписка на ежемесячный доступ к сервису gostlink.ru',
-                amount: price,
+                amount: subPrice,
                 currency: 'RUB',
                 accountId: localStorage.getItem('email'),
                 invoiceId: localStorage.getItem('uid'),
@@ -154,7 +156,6 @@ const Pricing = () => {
                         console.log("Response from Native: " + data);
                     }
                 );
-
             },
             function (reason, options) {
                 if (!window.kmpJsBridge) {
@@ -174,63 +175,117 @@ const Pricing = () => {
                         console.log("Response from Native: " + data);
                     }
                 );
+                navigate('/profile');
             });
     };
 
     return (
         <div className="row-main">
-            <div className="card">
-                <div className="card-header">
-                    <span className="discount-badge" style={{backgroundColor: '#e6f4ea', color: '#2f855a'}}>Скидка 40%</span>
-                    <h3 className="card-title">Подписка на 2 устройства</h3>
-                    <div className="price">
-                        <span className="price-value">299</span>
-                        <span className="price-symbol">₽/мес</span>
+            { shopData.success ? (
+                <div className="card">
+                    {shopData.firstPayment === shopData.monthlyPayment ? (
+                        <div className="card-header">
+                            <span className="discount-badge" style={{
+                                backgroundColor: '#e6f4ea',
+                                color: '#2f855a'
+                            }}>Скидка {shopData.discount}%</span>
+                            <h3 className="card-title">Месячная подписка</h3>
+                            <div className="price">
+                                <span className="price-value">{shopData.firstPayment} </span><br/>
+                                <span className="price-symbol">₽/мес</span>
+                            </div>
+                            <div className="price-old">{shopData.strikethroughPrice} ₽/мес</div>
+                        </div>
+                    ) : (
+                        <div className="card-header">
+                            <span className="discount-badge" style={{
+                                backgroundColor: '#e6f4ea',
+                                color: '#2f855a'
+                            }}>Скидка {shopData.discount}%</span>
+                            <h3 className="card-title">Месячная подписка</h3>
+                            <div className="price">
+                                <span className="price-value">Первый месяц {shopData.firstPayment} </span><br/>
+                                <span className="price-symbol">₽</span>
+                            </div>
+                            <div className="price">
+                                <span className="price-value">Далее {shopData.monthlyPayment} </span><br/>
+                                <span className="price-symbol">₽/мес</span>
+                            </div>
+                            <div className="price-old">{shopData.strikethroughPrice} ₽/мес</div>
+                        </div>
+                    )}
+                    <div className="card-body">
+                        <table>
+                            <tr><td><BsStars class="icon"/></td><td><span className="feature">∞ GB и без ограничений скорости</span></td></tr>
+                            <tr><td><BsStars class="icon"/></td><td><span className="feature">Доступ к иностранным сервисам</span></td></tr>
+                            <tr><td><BsStars class="icon"/></td><td><span className="feature">Работают российские банки</span></td></tr>
+                            <tr><td><BsStars class="icon"/></td><td><span className="feature">Без рекламы в приложении</span></td></tr>
+                        </table>
                     </div>
-                    <div className="price-old">498 ₽/мес</div>
-                </div>
-                <div className="card-body">
-                    <table>
-                        <tr><td><BsStars class="icon" /></td>
-                            <td><span className="feature">∞ GB/мес</span></td></tr>
-                        <tr><td><BsStars class="icon" /></td>
-                            <td><span className="feature">Безграничный доступ к иностранным сервисам</span></td></tr>
-                        <tr><td><BsStars class="icon" /></td>
-                            <td><span className="feature">Работают банки</span></td></tr>
-                        <tr><td><BsStars class="icon" /></td>
-                            <td><span className="feature">Без ограничений скорости</span></td></tr>
-                        <tr><td><BsStars class="icon" /></td>
-                            <td><span className="feature">Без рекламы</span></td></tr>
-                    </table>
-                    <button onClick={() => pay(299)} style={{marginTop: '30px', maxWidth: '300px', width: '100%'}} className="button_dark">
-                        Выбрать
-                    </button>
-                </div>
-            </div>
+                    {shopData.promoActivated === false ? (
+                        <>
+                        {isPromoInput ? (
+                            <div className="card-input">
+                                <div className="input-flex">
+                                    <span style={{marginRight: '5px'}}>Введи промокод: </span>
+                                    <input
+                                        className="promo-input"
+                                        type="text"
+                                        value={promo}
+                                        onChange={(e) => {
+                                            let newValue = e.target.value.toUpperCase().replace(/[^A-Z0-9]/g, '');
+                                            setPromo(newValue);
+                                        }}
+                                        inputMode="text"
+                                        onKeyDown={(e) => {
+                                            // console.log(e.key);
+                                            if (e.key === 'Enter') {
+                                                fetchPromo();
+                                            }
+                                        }}
+                                        onBlur={() => {
+                                            fetchPromo();
+                                        }}
+                                    />
+                                        <div className="go-icon" onClick={() => fetchPromo()}>
+                                            <HiOutlineArrowSmallRight
+                                                style={{height: '30px', width: '30px'}}/>
+                                        </div>
+                                </div>
+                                <div>
+                                    { msgFromPromo.length > 1 ? (
+                                        <span style={{color: 'red'}}>{msgFromPromo}</span>
+                                    ) : (
+                                        <></>
+                                    )}
+                                </div>
+                                </div>
+                                ) : (
+                                <div onClick={() => setIsPromoInput(true)} className="card-promotion">
+                                    <span>Есть промокод?</span>
+                                </div>
+                                )}
+                            </>
+                        ) : (
+                            <div onClick={() => setIsPromoInput(true)} className="card-promotion-complete">
+                            <span style={{color: 'green'}}>Промокод успешно активирован</span>
+                        </div>
+                    )}
+                    <div className="card-body">
+                        <button onClick={() => pay(shopData.firstPayment, shopData.monthlyPayment)}
+                                    style={{marginTop: '0', maxWidth: '300px', width: '100%'}}
+                                    className="button_dark">
+                                Оплатить
+                            </button>
+                        </div>
+                    </div>
+                    ) : (
+                    <span>Loading...</span>
+                    )}
 
-            {/*<div className="card">*/}
-            {/*    <div className="card-header">*/}
-            {/*        <span className="discount-badge" style={{backgroundColor: '#fefcbf', color: '#d69e2e'}}>Скидка 50%</span>*/}
-            {/*        <h3 className="card-title">Подписка на 3 устройства</h3>*/}
-            {/*        <div className="price">*/}
-            {/*            <span className="price-value">399</span>*/}
-            {/*            <span className="price-symbol">₽/мес</span>*/}
-            {/*        </div>*/}
-            {/*        <div className="price-old">799 ₽/мес</div>*/}
-            {/*    </div>*/}
-            {/*    <div className="card-body">*/}
-            {/*        <span className="feature"><BsStars/> ∞ GB/мес</span><br/>*/}
-            {/*        <span className="feature"><BsStars/> Безграничный доступ к иностранным сервисам</span><br/>*/}
-            {/*        <span className="feature"><BsStars/> Работают банки</span> <br/>*/}
-            {/*        <span className="feature"><BsStars/> Без ограничений скорости</span> <br/>*/}
-            {/*        <span className="feature"><BsStars/> Без рекламы</span>*/}
-            {/*        <button onClick={() => payYear(399)} style={{marginTop: '30px'}}*/}
-            {/*                className="button_dark">Выбрать*/}
-            {/*        </button>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-        </div>
-    );
-}
+                </div>
 
-export default Pricing;
+            );
+            }
+
+            export default Pricing;
